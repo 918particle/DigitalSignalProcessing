3774ad5d5a1ba025f5d50156466125d143cc997a
Jordan C Hanson <918particle@gmail.com>
2019-01-22 13:58:18 -0800

    Added example script for recursive filter.

--- Unit2/code/single_pole_low_pass_recursive.m
+++ Unit2/code/single_pole_low_pass_recursive.m
@@ -4,42 +4,52 @@ home;
 pkg load signal;
 
 %dsp parameters for x-axis:
-fs = 1000.0; %Hz
-f_sig = 75; %Hz
-T = 20.0; %Total time (1 sec)
+fs = 10000.0; %Hz
+T = 2.0; %Total time
 dt = 1/fs;
 df = 1/T;
-f = transpose([0:df:fs/2]);
+f = transpose([df:df:fs/2+df]);
 t = transpose([0.0:dt:T-dt]);
 N = length(t)/2;
 %dsp parameters for y-axis:
 noise_sigma = 1.0;
+amplitude = 2.0; %An SNR of only 2.0!
 noise = randn(size(t))*noise_sigma;
-x = exp(-2*pi*0.06);
-a = [1 1-x];
-b = [x x];
+x = exp(-2*pi*0.025);
+a = 1-x;
+b = x;
+%signals
+y_in = zeros(size(t));
+y_in(1000:1300) = amplitude;
+y_in(1301:1600) = -amplitude;
+y_in = y_in+noise;
+y_out = zeros(size(y_in));
 
 %filtering
-y = filter(b,a,noise);
+n = length(y_in);
+
+for i=2:n
+	y_out(i) = a*y_in(i)+b*y_out(i-1);
+endfor
 
 %Computing the spectrum
-Y = sqrt(conj(fft(y)).*fft(y));
+Y = sqrt(conj(fft(y_out)).*fft(y_out));
 Y_R = Y(1:length(f))*dt;
 Y_L = Y(length(f)-1:end)*dt;
 
 %Plotting section
 figure(1, 'position',[0,0,1000,1000]);
 subplot(2,1,1);
-plot(t,y,'color','black','linewidth',2.0);
+plot(t,y_out,'color','black','linewidth',2.0);
 xlabel('Time (seconds)','fontname','Courier','fontsize',20);
 ylabel('Signal (Volts)','fontname','Courier','fontsize',20);
 set(gca(),'fontname','Courier','fontsize',20);
 axis([0-dt*50 T+dt*50 -5*noise_sigma 5*noise_sigma]);
 subplot(2,1,2);
 hold on;
-plot(f,20*log10(Y_R),'color','black','linewidth',2.0);
-plot(f,20*log10(flipud(Y_L)),'o','color','blue','linewidth',2.0);
-axis([0-df*50 fs/2+df*50 -50 10]);
+semilogx(f,20*log10(Y_R),'color','black','linewidth',2.0);
+semilogx(f,20*log10(flipud(Y_L)),'o','color','blue','linewidth',2.0);
+axis([df fs/2+df*50 -70 20]);
 xlabel('Frequency (Hz)','fontname','Courier','fontsize',20);
 ylabel('20*log_{10}(Signal (V/Hz))','fontname','Courier','fontsize',20);
 set(gca(),'fontname','Courier','fontsize',20);
