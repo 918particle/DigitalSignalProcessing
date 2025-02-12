45790d6aa828a6f7af70a5863ad64fe3c9f5004f
Jordan C hanson <918particle@gmail.com>
2025-02-11 18:29:30 -0800

    updated warm ups and code for lab

--- Unit1/code/Aliasing.m
+++ Unit1/code/Aliasing.m
@@ -17,10 +17,20 @@ function retval = fourier_square(n,x)
 	endfor
 endfunction
 
+%Prediction, for plotting
+function retval = predict(f)
+	retval = zeros(size(f));
+	retval(1) = 0.5;
+	n = length(f);
+	for i=2:n
+		retval(i) = 2.0/pi/i;
+	endfor
+endfunction
+
 %dsp parameters for x-axis:
 fs = 1000.0; %Hz
 f_0 = 4; %Hz
-T = 10.0; %Total time (10 sec)
+T = 2.0; %Total time (10 sec)
 dt = 1/fs;
 df = 1/T;
 f = transpose([0:df:fs/2-df]);
@@ -28,7 +38,7 @@ t = transpose([0.0:dt:T-dt]);
 N = length(t)/2;
 %dsp parameters for y-axis:
 amplitude = 1;
-n_modes = 50;
+n_modes = 25;
 noise_sigma = 0.001;
 noise = randn(size(t))*noise_sigma;
 y = amplitude*fourier_square(n_modes,2*pi*f_0*t)+noise;
@@ -41,19 +51,20 @@ sum(Y.^2)/length(t)
 
 Y_R = Y(1:length(f))*dt;
 Y_L = Y(length(f)+1:end)*dt;
-
+P = predict(f);
+P = P*max(Y_R)/max(P);
 spectrum = 20*log10(Y_R);
-prediction = 20*log10((N/fs)*2./(pi*(f/f_0)));
-spectrum = spectrum - max(spectrum);
-prediction = prediction - 13.0;
+prediction = 20*log10(P);
+spectrum = spectrum-max(spectrum);
+prediction = prediction+13.0;
 
 %Plotting section
-figure(1, 'position',[0,0,1000,1000]);
+figure(1, 'position',[0,0,650,650]);
 subplot(2,1,1);
 plot(t,y,'color','black','linewidth',2.0);
 xlabel('Time (seconds)','fontname','Courier','fontsize',16);
 ylabel('Amplitude (Volts)','fontname','Courier','fontsize',16);
-set(gca(),'fontname','Courier','fontsize',20);
+set(gca(),'fontname','Courier','fontsize',16);
 axis([0-dt*50 T+dt*50 -0.5 amplitude*1.5]);
 subplot(2,1,2);
 hold on;
@@ -68,4 +79,4 @@ set(gca(),'fontname','Courier','fontsize',16);
 box on;
 
 %Printint to PDF
-%print -dpdf "octave1.pdf"
+print -dpdf "octave1.pdf"
