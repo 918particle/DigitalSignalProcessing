bfe00dc45db7fd9871e6f08f88652af2720d7272
Jordan C hanson <918particle@gmail.com>
2025-02-20 14:16:44 -0800

    new warm up and code lab 5

--- Unit1/code/impulse_response.m
+++ Unit1/code/impulse_response.m
@@ -4,23 +4,24 @@ home;
 pkg load signal;
 
 %dsp parameters for x-axis:
-fs = 1000.0; %Hz
-f_sig = 75; %Hz
-T = 20.0; %Total time (1 sec)
+fs = 2.0e4; %Hz
 dt = 1/fs;
+T = 1e3*dt;
 df = 1/T;
 f = transpose([0:df:fs/2]);
-t = transpose([0.0:dt:T-dt]);
-N = length(t)/2;
+t = transpose([dt:dt:T]);
+
 %dsp parameters for y-axis:
-amplitude = 1.0;
-noise_sigma = 1.0;
+amplitude = 2.0;
+noise_sigma = 1e-1;
 noise = randn(size(t))*noise_sigma;
-y = amplitude*sin(2.0*pi*f_sig.*t)+noise;
+y = zeros(size(t));
+y(1) = amplitude;
+y = y+noise;
 
 %filtering
-%[b1,a1] = butter(8,0.16);
-%[b2,a2] = butter(8,0.15,"high");
+%[b1,a1] = butter(8,0.5,"low");
+%[b2,a2] = butter(8,0.5,"high");
 %y = filter(b1,a1,y); %low-pass
 %y = filter(b2,a2,y); %high-pass
 
@@ -36,12 +37,12 @@ plot(t,y,'color','black','linewidth',2.0);
 xlabel('Time (seconds)','fontname','Courier','fontsize',20);
 ylabel('Signal (Volts)','fontname','Courier','fontsize',20);
 set(gca(),'fontname','Courier','fontsize',20);
-axis([0-dt*10 T+dt*10 -amplitude*10 amplitude*10]);
+axis([0-dt T+dt -amplitude*1.1 amplitude*1.1]);
 subplot(2,1,2);
 hold on;
 plot(f,20*log10(Y_R),'color','black','linewidth',2.0);
 plot(f,20*log10(flipud(Y_L)),'o','color','blue','linewidth',2.0);
-axis([0-df*50 fs/2+df*50 -50 50]);
+axis([0-df*10 fs/2+df*10 -120 0]);
 xlabel('Frequency (Hz)','fontname','Courier','fontsize',20);
 ylabel('20*log_{10}(Signal (V/Hz))','fontname','Courier','fontsize',20);
 set(gca(),'fontname','Courier','fontsize',20);
