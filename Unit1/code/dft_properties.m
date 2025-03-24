532d35be333cb1c4b8e9697c1bc6dd7bc27b7768
Jordan C hanson <918particle@gmail.com>
2025-03-24 15:57:33 -0700

    added quiz 1 results

--- Unit1/code/dft_properties.m
+++ Unit1/code/dft_properties.m
@@ -4,14 +4,15 @@ home;
 
 fs = 1e3;
 dt = 1/fs;
-totalT = 100.0;
+totalT = 20.0;
 t = dt:dt:totalT;
 t0 = 1.0;
-T = 0.0725;
+T = 0.1;
 
 function retval = sP(x,t0,T)
+  dt = x(2)-x(1);
   retval = zeros(size(x));
-  retval(find(and(x>t0-T/2,x<t0+T/2))) = 1.0;
+  retval(find(and(x>t0-T/2,x<t0+T/2-dt))) = 1.0;
 endfunction
 
 function [f,S] = set_spectrum(t,t0,T,fs)
@@ -24,17 +25,21 @@ function [f,S] = set_spectrum(t,t0,T,fs)
 endfunction
 
 function retval = dft_sync(f,N,M)
-k = 0:length(f);
-retval = zeros(k,1);
-for i=[0:N]
-retval(i) = abs(sin(pi*i*M/N)/sin(pi*i/N));
-endfor
+  kmax = length(f);
+  retval = zeros(kmax,1);
+  for i=[1:kmax]
+    retval(i) = abs(sin(pi*i*M/N)/sin(pi*i/N));
+  endfor
 endfunction
 
 sig = sP(t,t0,T);
 [f,S] = set_spectrum(t,t0,T,fs);
 N = length(S);
-
 parseval_theorem_check = [sum(sig.^2) sum(S.^2)/N];
 
-
+N = length(t);
+M = length(find(sig>0));
+sync_spec = dft_sync(f,N,M);
+plot(f,sync_spec,'color','red')
+hold on
+plot(f,S,'--','color','black')
