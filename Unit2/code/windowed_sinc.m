f4b107b217ccf659b9a874672af24138aa929dae
Jordan C hanson <918particle@gmail.com>
2025-04-10 10:46:40 -0700

    grading update, midterm 2, code labs

--- Unit2/code/windowed_sinc.m
+++ Unit2/code/windowed_sinc.m
@@ -1,40 +1,37 @@
 clear;
 close;
 home;
-pkg load signal;
 
-%dsp parameters for x-axis:
-fs = 10.0e3; %Hz
-f_sig = 1200; %Hz
-T = 1.0; %Total time (1 sec)
+fs = 1.0e4;
+fcl = 2.6e3;
+fch = 2.4e3;
+fcl = fcl/fs;
+fch = fch/fs;
 dt = 1/fs;
-t = transpose([dt:dt:T]);
-%dsp parameters for y-axis:
-amplitude = 1.0;
-noise_sigma = 1.0;
-noise = randn(size(t))*noise_sigma;
-y = noise;
-
-%Low-pass filtering
-M = 100;
-i = [1:M];
-fc = f_sig/fs;
-lp_kernel = transpose(sin(2*pi*fc.*(i-M/2))./(i-M/2)).*blackman(M);
-lp_kernel(end/2) = 2*pi*fc;
-lp_kernel = conv(lp_kernel,lp_kernel);
-lp_kernel = lp_kernel/(sqrt(sum(lp_kernel.*lp_kernel)));
-z = conv(y,lp_kernel);
-
-%Computing the filtered spectrum
-Z = sqrt(conj(fft(z)).*fft(z));
-Z = Z(1:end/2)*dt;
+T = 1.0;
+y = randn(size(dt:dt:T));
+y = y/sqrt(sum(y.*y));
+N = 201;
+n = 0:N-1;
+alpha = (N-1)/2;
+%Low-pass filter design
+h_lp = sin(2*pi*fcl*(n-alpha))./(pi*(n-alpha));
+h_lp(alpha+1) = 2*fcl;
+h_lp = transpose(h_lp).*blackman(N);
+%High-pass filter design
+h_hp = sin(2*pi*fch*(n-alpha))./(pi*(n-alpha));
+h_hp(alpha+1) = 2*fch;
+h_hp = transpose(h_hp).*blackman(N);
+h_hp = -h_hp;
+h_hp(alpha+1) = 1+h_hp(alpha+1);
+%DFT computation
+z = conv(conv(y,h_hp),h_lp);
+Z = fft(z);
+Z = 20*log10(abs(Z(1:end/2)*dt));
 f = linspace(0,fs/2,length(Z));
-
-%Plotting section
-figure(1, 'position',[0,0,1000,1000]);
-plot(f,20*log10(Z),'color','black','linewidth',2.0);
-axis([0 fs/2 -250 10]);
-xlabel('Frequency (Hz)','fontname','Courier','fontsize',20);
-ylabel('Amplitude (dB/Hz)','fontname','Courier','fontsize',20);
+%Plotting
+plot(f,Z)
 set(gca(),'fontname','Courier','fontsize',20);
-box on;
+xlabel('Frequency (Hz)');
+ylabel('Signal (dB/Hz)');
+axis([0 fs/2 -200 0]);
